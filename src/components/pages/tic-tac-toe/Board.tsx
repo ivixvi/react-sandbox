7e385f1afb429c80e8bc0b3265e2659e4bc4897c
ivixvi <epitus628@gmail.com>
2020-12-20 21:20:14 +0900

    add: judge game over

--- src/components/pages/tic-tac-toe/Board.tsx
+++ src/components/pages/tic-tac-toe/Board.tsx
@@ -7,22 +7,58 @@ function changeBoard(board: Array<string>, state: string, position: number): Arr
     return tmpboard
 }
 
+function calculateWinner(squares: Array<string>): string {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return '';
+}
 
 export function Board(props: any) {
+    const [isGameOver, setIsGameOver] = useState(false)
     const [nextPalayer, setNextPlayer] = useState(true)
+    const [state, setState] = useState(`NextPlayer: ${nextPalayer? '○': '×'}`)
     const [board, setBoard] = useState(Array(9).fill(''))
     
     function squareClickHandler(position: number) {
+        // すでに終了している場合は処理を行わない
+        if(isGameOver) return void 0;
         // すでにクリック済みの場合は処理を行わない
         if (board[position] !== '') return void 0;
         
-        setBoard(changeBoard(board,nextPalayer? '○': '×',position))
+        const nextBoard = changeBoard(board,nextPalayer? '○': '×',position)
+        setBoard(nextBoard)
+        
+        const winner = calculateWinner(nextBoard)
+        
+        // Winnerが決まった際にStateの変更と盤面固定を行う
+        if(winner !== ''){
+            setState(`Winner is ${winner}`)
+            setIsGameOver(true)
+            return void 0
+        }
+        
+        // 手番を交代する
         setNextPlayer(!nextPalayer)
+        setState(`NextPlayer: ${!nextPalayer? '○': '×'}`)
     }
 
     return (
         <div>
-        <div className="status">Next Player: {nextPalayer? '○': '×'}</div>
+        <div className="status">{state}</div>
         <div className="board-row">
             <Square value={board[0]} onClick={()=>{ squareClickHandler(0)}}/>
             <Square value={board[1]} onClick={()=>{ squareClickHandler(1)}}/>
